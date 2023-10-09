bdee36c59bdce9844a160738a73504e4f819a813
Hasini Perera <it21036248@my.sliit.lk>
2023-10-09 17:42:32 +0530

    update

--- PredictionApp.py
+++ PredictionApp.py
@@ -3,29 +3,20 @@ import pandas as pd
 import pickle
 
 # Load the Random Forest model from the pickle file
-try:
-    model = pickle.load(open('grid_search_rf_model.pkl', 'rb'))
-except Exception as e:
-    print(f"Error loading the model: {str(e)}")
+model = pickle.load(open('grid_search_rf_model.pkl', 'rb'))
 
-
-# Create a function to preprocess user input and make predictions
 
 
 def predict_churn_status(input_data):
     # Preprocess the input data
     input_df = pd.DataFrame([input_data])
-    
+    #st.write(input_data)
     # Make predictions using the loaded model
-    try:
-        prediction = model.predict(input_df)
-    except Exception as e:
-        print(f"Error during prediction: {str(e)}")
-
-    
+    prediction = model.predict(input_df)
+    # probability = model.predict_proba(input_df)[:, 1]
 
     return "Approved" if prediction[0] == 1 else "Rejected"
-    
+    # return prediction[0], probability[0]
 
 # Create the Streamlit app
 
